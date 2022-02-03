37de5ae77497e3187eadada84ef1caad6a958dab
Bryan <50989230+bryan0x17@users.noreply.github.com>
2022-02-03 14:25:50 -0700

    Update HomeServlet.java

--- lab4/src/main/java/ca/sait/lab4/servlets/HomeServlet.java
+++ lab4/src/main/java/ca/sait/lab4/servlets/HomeServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 public class HomeServlet extends HttpServlet {
@@ -22,6 +23,11 @@ public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String username = (String) session.getAttribute("username");
+        request.setAttribute("username", username);
+        getServletContext().getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
+        
     }
 
     /**
